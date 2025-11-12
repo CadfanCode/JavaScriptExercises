48a64f94509988057b75274ef5f751267ee19191
Cai <caiowain@gmail.com>
2025-11-12 01:48:16 +0000

    sixth commit - second exercise

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-// Change text in paragraph to something else.
+// Exercise 1: Change text in the paragraph to something else.
 const buttonHandle = document.getElementById("btn1");
 const textHandle = document.querySelector("#p1");
 buttonHandle.addEventListener('click', () =>{
     textHandle.textContent = "You clicked on the button!";
+})
+
+// Change the colour of the background.
+const backgroundButton = document.getElementById("btn2");
+const backgroundHandle = document.querySelector("body");
+backgroundButton.addEventListener('click', ()=>{
+    backgroundHandle.style.color = Math.random(color);
 })
