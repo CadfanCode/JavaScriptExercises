e8726a250cc021e9be4da7e5161d21d42b8f611a
Cai <caiowain@gmail.com>
2025-11-12 01:23:16 +0000

    fifth commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Change text in paragraph to something else.
-const buttonHandle = document.getElementById("btn1"); // Get a handle on the button
+const buttonHandle = document.getElementById("btn1");
 const textHandle = document.querySelector("#p1");
 buttonHandle.addEventListener('click', () =>{
     textHandle.textContent = "You clicked on the button!";
