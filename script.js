f4a00c254e27c671f67514d7d2fe492446571997
Cai <caiowain@gmail.com>
2025-11-12 04:19:23 +0100

    11th

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttonHandle = document.getElementById("btn1");
 const textHandle = document.querySelector("#p1");
 buttonHandle.addEventListener('click', () =>{
     textHandle.textContent = "You clicked on the button!";
-})
+});
 
 // Exercise 2: Change the colour of the background.
 function getRandomColor() {
@@ -13,20 +13,22 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 
 const backgroundButton = document.getElementById("btn2");
 const backgroundHandle = document.querySelector("body");
 
 backgroundButton.addEventListener('click', () => {
     backgroundHandle.style.backgroundColor = getRandomColor();
-})
+});
 
 // Excercise 3: Write out users input.
 const submitButton = document.getElementById("btn3");
 const inputTextfield = document.getElementById("textfield");
 
-submitButton.addEventListener('click',() =>{
+submitButton.addEventListener('click',() => {
     const outputTextfield = document.getElementById("p2");
     outputTextfield.textContent = inputTextfield.value;
-})
+});
+
+// Exercise 4: Switch themes
